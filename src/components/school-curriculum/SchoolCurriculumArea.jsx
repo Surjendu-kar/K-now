c3c3ff8e61f0617a9fcd3e2a6ffbea201a0fee8a
Surjendu kar <rahulgotp9988@gmai.com>
2025-02-17 17:05:50 +0530

    Implement school curriculum section with dynamic content rendering and responsive grid layout

--- src/components/school-curriculum/SchoolCurriculumArea.jsx
+++ src/components/school-curriculum/SchoolCurriculumArea.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React from "react";
+import { schoolCurriculum } from "@/src/data/school_curriculum.json";
+import Link from "next/link";
 
 function SchoolCurriculumArea() {
   return (
-    <div>SchoolCurriculumArea</div>
-  )
+    <>
+      <section
+        className="category-ractangle-area pb-90 pt-90 wow fadeInUp"
+        data-wow-duration=".8s"
+        data-wow-delay=".4s"
+      >
+        <div className="container">
+          <div className="row">
+            {schoolCurriculum.map((item) => (
+              <div key={item.id} className="col-xl-3 col-lg-4 col-md-6 col-12">
+                <div className="tp-cat-item tp-rec-item mb-35">
+                  <div className="tp-category-icon mb-30">
+                    <img src={item.icon} alt="category-img" />
+                  </div>
+                  <h4 className="tp-category-title tp-r-cat-title">
+                    <Link href="/#">{item.name}</Link>
+                  </h4>
+                  <p>{item.courses}</p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default SchoolCurriculumArea
+export default SchoolCurriculumArea;
