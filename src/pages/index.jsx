bfd7a06c0800faf25f3b8655d63125ecb319cf75
Surjendu kar <rahulgotp9988@gmai.com>
2025-02-28 21:42:03 +0530

    Update SEO page title from 'Iqnaut' to 'K-now'

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,7 +6,7 @@ import Wrapper from "../layout/wrapper";
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle={'Iqnaut'} />
+      <SEO pageTitle={'K-now'} />
       <Home />
     </Wrapper>
   );
